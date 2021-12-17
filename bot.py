eea0485020c136edd9d14aeaa30895d8a79a6e2b
annanasnas <90215820+annanasnas@users.noreply.github.com>
2021-12-17 16:43:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,7 +146,7 @@ def follow(update, context):
   '''
     Функция, отображающая логику напоминаний о дедлайнах
   '''
-  update.message.reply_text("Начиная ботать. Я слежу")
+  update.message.reply_text("Начинай ботать. Я слежу")
   schedule.every(60).minutes.do(partial(job, update, context)).tag('deadlines')
   schedule.every(90).minutes.do(partial(job2, update, context)).tag('deadlines')
   schedule.every(30).minutes.do(partial(show_deadlines, update, context)).tag('deadlines')
