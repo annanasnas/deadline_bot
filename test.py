8385859911789c094d24e17c509e1cb4322b3cd4
annanasnas <anastasiyasurakshina@yandex.ru>
2021-12-17 18:24:40 +0300

    fix

--- test.py
+++ test.py
@@ -1,23 +1,21 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 import bot
 
 class BotCase(unittest.TestCase):
   def test_first_response_no(self):
-    chat = "Нет, отстань"
-    message = MagicMock(chat=chat)
-    update = MagicMock(message=message)
-    context = MagicMock(user_data=dict())
+    message = "Нет, отстань"
+    update = MagicMock(text=message)
+    context = MagicMock()
     bot.first_response(update, context)
     message.reply_text.assert_called_with("Ну как хочешь...")
   def test_first_response_yes(self):
-    chat = "Да, мне нужна помощь"
-    message = MagicMock(chat=chat)
-    update = MagicMock(message=message)
-    context = MagicMock(user_data=dict())
+    message = "Да, мне нужна помощь"
+    update = MagicMock(text=message)
+    context = MagicMock()
     bot.first_response(update, context)
-    message.reply_text.assert_called_with("Отлично, я помогу тебе!")
-    message.reply_text.assert_called_with("Введи название дедлайна")
+    calls = [call("Отлично, я помогу тебе!"), call("Введи название дедлайна")]
+    message.reply_text.assert_has_calls(calls)
 
 if __name__ == '__main__':
   unittest.main()
